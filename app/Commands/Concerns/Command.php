f24c8febc4647eabe722d4ae30746f0dce38e9fd
Caen De Silva <caen@desilva.se>
2024-03-04 11:28:43 +0100

    Construct terminal instance to base command

--- app/Commands/Concerns/Command.php
+++ app/Commands/Concerns/Command.php
@@ -3,16 +3,19 @@
 namespace App\Commands\Concerns;
 
 use App\Git;
+use Symfony\Component\Console\Terminal;
 
 abstract class Command extends \LaravelZero\Framework\Commands\Command
 {
     protected Git $git;
+    protected Terminal $terminal;
 
     public function __construct(Git $git)
     {
         parent::__construct();
 
         $this->git = $git;
+        $this->terminal = new Terminal();
     }
 
     public function fatal(string $message): int
